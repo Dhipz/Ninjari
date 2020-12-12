45e71aae8ffbb6f39a78d9cb48d50e4a4b9d4eb9
PRADHIPTA TRIMANGGALA SATYA IPB <pradhipta_ts@apps.ipb.ac.id>
2020-12-12 08:52:57 +0000

    basic player movement

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody2D playerRigidBody;
+	private Collider2D playerCollider;
+
+	public float moveSpeed;
+	public float jumpPower;
+	private bool isJump;
+	private bool onGround;
+
+	public LayerMask groundLayer;
+	// Start is called before the first frame update
+	void Start()
+	{
+		playerRigidBody = GetComponent<Rigidbody2D>();
+		playerCollider = GetComponent<Collider2D>();
+		isJump=false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+			isJump = true;
+		}
+		onGround = Physics2D.IsTouchingLayers(playerCollider, groundLayer);
+	}
+
+	// Update on fixed tick for physic
+	void FixedUpdate()
+	{
+		if(isJump)
+		{
+			if(onGround)
+			{
+				playerRigidBody.velocity = new Vector2(moveSpeed, jumpPower);
+			}
+		}
+		else
+		{
+			playerRigidBody.velocity = new Vector2(moveSpeed, playerRigidBody.velocity.y);
+		}
+		isJump = false;
+	}
+}
