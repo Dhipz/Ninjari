7fa1ca202abc2494118bc558839c7fee84e7ec7b
aryaaminata93ipb <arya_aminata93@apps.ipb.ac.id>
2020-12-24 09:25:39 +0000

    ground generator

---
+++ Assets/Script/GroundGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+    public GameObject ground;
+    public Transform pointbatas;
+    public float distanceground;
+
+    private float groundlength;
+
+    public float distancegroundmin;
+    public float distancegroundmax;
+
+    //public ObjectPooler theObjectPool;
+
+    void Start()
+    {
+        groundlength = ground.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < pointbatas.position.x)
+        {
+            //distanceground = Random.Range(distancegroundmin, distancegroundmax);
+            transform.position = new Vector3(transform.position.x + groundlength + distanceground, transform.position.y, transform.position.z);
+
+            Instantiate (ground, transform.position, transform.rotation);
+            //GameObject newPlatform = theObjectPool.GetPooledObject();
+
+            //newPlatform.transform.position = transform.position;
+            //newPlatform.transform.rotation = transform.rotation;
+            //newPlatform.SetActive(true);
+
+        }
+    }
+}
