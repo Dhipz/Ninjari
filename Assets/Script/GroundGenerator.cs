89fdf71f0e8b89f9530e3d65f055b86fbecacf09
aryaaminata93ipb <arya_aminata93@apps.ipb.ac.id>
2020-12-24 12:17:12 +0000

    random platform type 1.0

--- Assets/Script/GroundGenerator.cs
+++ Assets/Script/GroundGenerator.cs
@@ -13,11 +13,20 @@ public class GroundGenerator : MonoBehaviour
     public float distancegroundmin;
     public float distancegroundmax;
 
-    public ObjectPooler theObjectPool;
+    public GameObject[] grounds;
+    private int groundselector;
+    private float[] groundslength;
+
+    //public ObjectPooler theObjectPool;
 
     void Start()
     {
-        groundlength = ground.GetComponent<BoxCollider2D>().size.x;
+        //groundlength = ground.GetComponent<BoxCollider2D>().size.x;
+        groundslength = new float[grounds.Length];
+
+        for(int i=0; i<grounds.Length; i++){
+            groundslength[i] = grounds[i].GetComponent<BoxCollider2D>().size.x;
+        }
     }
 
     // Update is called once per frame
@@ -26,14 +35,18 @@ public class GroundGenerator : MonoBehaviour
         if(transform.position.x < pointbatas.position.x)
         {
             distanceground = Random.Range(distancegroundmin, distancegroundmax); //random jarak ground
-            transform.position = new Vector3(transform.position.x + groundlength + distanceground, transform.position.y, transform.position.z);
+            
+            groundselector = Random.Range(0, grounds.Length);
+
+            transform.position = new Vector3(transform.position.x + groundslength[groundselector] + distanceground, transform.position.y, transform.position.z);
 
             //Instantiate (ground, transform.position, transform.rotation);
-            GameObject newPlatform = theObjectPool.GetPooledObject();
+            Instantiate (grounds[groundselector], transform.position, transform.rotation);
+            //GameObject newPlatform = theObjectPool.GetPooledObject();
 
-            newPlatform.transform.position = transform.position;
-            newPlatform.transform.rotation = transform.rotation;
-            newPlatform.SetActive(true);
+            //newPlatform.transform.position = transform.position;
+            //newPlatform.transform.rotation = transform.rotation;
+            //newPlatform.SetActive(true);
 
         }
     }
