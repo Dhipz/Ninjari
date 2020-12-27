f925df3e8a1bf68982ba5bcc794a5c1fcad20c1b
PRADHIPTA TRIMANGGALA SATYA IPB <pradhipta_ts@apps.ipb.ac.id>
2020-12-27 03:08:02 +0000

    Pause Button fix

--- Assets/Script/DeathMenu.cs
+++ Assets/Script/DeathMenu.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 public class DeathMenu : MonoBehaviour
 {
     public string mainMenu;
-    
+    public GameObject pauseButton;
+    void OnEnable(){
+    	pauseButton.SetActive(false);
+    }
     public void RestartGame(){
         FindObjectOfType<GameManager>().Reset();
+        pauseButton.SetActive(true);
     }
     public void QuitMain(){
         SceneManager.LoadScene(mainMenu);
--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -7,18 +7,32 @@ public class PauseMenu : MonoBehaviour
 {
     public string mainMenu;
     public GameObject pauseMenu;
+    public GameObject pauseButton;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.P)){
+            PauseGame();
+        }
+    }
     public void PauseGame(){
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+        /*while(true){
+            if(Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.P)){
+                ResumeGame();
+            }
+        }*/
     }
     public void ResumeGame(){
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
     }
     
     public void RestartGame(){
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
         FindObjectOfType<GameManager>().Reset();
     }
     public void QuitMain(){
