6101bacfc86c1c38a5347cc3fd661075ecd2b25f
aryaaminata93ipb <arya_aminata93@apps.ipb.ac.id>
2020-12-28 04:56:46 +0000

    update gui lagi

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{
+{   
+    public GameObject rulesWindow;
+
     public string playGames;
     public void PlayGame(){
         SceneManager.LoadScene(playGames);
     }
+
+    public void RulesGame(){
+        rulesWindow.SetActive(true);
+    }
+
+    public void QuitRulesGame(){
+        rulesWindow.SetActive(false);
+    }
+
     public void QuitGame(){
         Application.Quit();
     }
